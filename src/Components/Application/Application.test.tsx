de86e2a7bbd18fdc9a75950c54384413afc9d5de
jmilind1234 <jmilind1234@gmail.com>
2026-04-14 08:45:31 +0530

    seen the options passed to getByRole REL query, name and level.

--- src/Components/Application/Application.test.tsx
+++ src/Components/Application/Application.test.tsx
@@ -3,11 +3,29 @@ import { Application } from "./Application";
 
 describe("Application", () => {
   test("renders correctly", () => {
-    render(<Application/>);
+    render(<Application />);
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading",{
+        level:2
+    })
+    expect(sectionHeading).toBeInTheDocument();
 
     // textbox is the aria-role for input tag with type text
-    const name = screen.getByRole('textbox');
+    const name = screen.getByRole("textbox", {
+      name: "Name",
+    }); // 'Name' is the content of the label for HTMLElement
     expect(name).toBeInTheDocument();
+
+    //textbox is also the default aria-role of the textarea
+    const bio = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bio).toBeInTheDocument();
 
     // combobox is the aria-role for the select tag
     const jobLocationElement = screen.getByRole("combobox");
